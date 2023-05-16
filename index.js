6a18a2049c107843ebb4b54470e5a54880ca2a2c
matthewm588 <matthewj.morelli@gmail.com>
2023-05-15 23:34:06 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(`User ${socket.id} connected`)
+
+    socket.on('join_room', (data) => {
+        socket.join(data);
+    });
+
+    socket.on('update_grid', (data) => {
+        socket.to(data.room).emit('receive_grid', data);
+    });
+
+    socket.on('update_bpm', (data) => {
+        socket.to(data.room).emit('receive_bpm', data);
+    });
+});
+
+server.listen(8080, () => {
+    console.log("Server is running.");
+});
